e7d1546fd3099cd8119538ec513a71a054ddc2c9
rayanekaraouzene <rayane.karaouzene@efrei.net>
2023-11-12 00:47:19 +0100

    Un peu de changement dans text process

--- utils/text-process.py
+++ utils/text-process.py
@@ -2,6 +2,8 @@ import os
 
 file_list = []
 president_list = []
+president_list_sorted = []
+president_list_sorted_nodupe = []
 def Nameextraction():
     for files in os.listdir('../speeches 2023/'):
         file_list.append(files)
@@ -9,8 +11,10 @@ def Nameextraction():
         president_name = file_list[i].replace('Nomination_','').replace('.txt','')
         president_list.append(president_name)
     print(president_list)
-
-
-
+    for i in president_list:
+        i = ''.join(filter(lambda z: not z.isdigit(), i))
+        president_list_sorted.append(i)
+    president_list_sorted_nodupe = list(dict.fromkeys(president_list_sorted))
+    print(president_list_sorted_nodupe)
 
 Nameextraction()
