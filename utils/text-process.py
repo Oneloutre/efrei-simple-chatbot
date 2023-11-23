e65fbdd92d34b7c58326c01f402e6b0bca04852f
Onelots <54822802+Oneloutre@users.noreply.github.com>
2023-11-23 17:54:00 +0100

    :rocket:  Name Extraction now works + reworked
    Signed-off-by: Onelots <54822802+Oneloutre@users.noreply.github.com>

--- utils/text-process.py
+++ utils/text-process.py
@@ -8,13 +8,30 @@ the_cleaned_folder_directory = f'../{the_cleaned_folder_name}'
 
 file_list = []
 president_list = []
+president_list_sorted = []
 def Nameextraction():
     for files in os.listdir('../speeches 2023/'):
         file_list.append(files)
     for i in range(len(file_list)):
         president_name = file_list[i].replace('Nomination_','').replace('.txt','')
         president_list.append(president_name)
-    print(president_list)
+    for i in president_list:
+        i = ''.join(filter(lambda z: not z.isdigit(), i))
+        if "Chirac" in i:
+            i = "Jacques Chirac"
+        if "Sarkozy" in i:
+            i = "Nicolas Sarkozy"
+        if "Hollande" in i:
+            i = "François Hollande"
+        if "Macron" in i:
+            i = "Emmanuel Macron"
+        if "Mitterrand" in i:
+            i = "François Mitterrand"
+        if "Giscard" in i:
+            i = "Valéry Giscard d'Estaing"
+        president_list_sorted.append(i)
+    president_list_sorted_nodupe = list(dict.fromkeys(president_list_sorted))
+    print(president_list_sorted_nodupe)
 
 
 def remove_punct(c):
